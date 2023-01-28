a0a8e68478131b840df2e96c837039663fc08220
Safwan Shamsir <103097574+safwanshamsir99@users.noreply.github.com>
2023-01-28 12:04:57 +0800

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -5,10 +5,9 @@ Created on Sat Jan 28 11:32:02 2023
 @author: Acer
 """
 
-import os
 import pickle
 
-MODEL_PATH = os.path.join(os.getcwd(),'best_model_covid.pkl')
+MODEL_PATH = 'best_model_covid.pkl'
 
 def predict(temp):
     with open(MODEL_PATH,'rb') as file:
