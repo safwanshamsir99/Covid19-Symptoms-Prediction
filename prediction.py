3b53f009da07a16e37275b7ccc85f32f8416dc14
Safwan Shamsir <103097574+safwanshamsir99@users.noreply.github.com>
2023-01-28 13:04:05 +0800

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -5,11 +5,8 @@ Created on Sat Jan 28 11:32:02 2023
 @author: Acer
 """
 
-import pickle
-
-MODEL_PATH = 'best_model_covid.pkl'
+import joblib
 
 def predict(temp):
-    with open(MODEL_PATH,'rb') as file:
-        model = pickle.load(file)
+    model = joblib.load('best_model_covid.sav')
     return model.predict(temp)
